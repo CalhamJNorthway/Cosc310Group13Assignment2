1beb826c32f58dbe27d88638b517b3dede15991d
Chris <chris.beda@gmail.com>
2019-02-08 18:03:27 -0800

    Searching the DictionaryTree and the unit testing is completed

--- src/main/java/chadbot/bot/dictionarytree/DictionaryTree.java
+++ src/main/java/chadbot/bot/dictionarytree/DictionaryTree.java
@@ -4,9 +4,10 @@ import chadbot.bot.data.ResponseTemplate;
 
 import java.util.List;
 
+@SuppressWarnings("WeakerAccess")
 public class DictionaryTree {
 
-    private static final String NO_RESPONSE = "";
+    public static final String NO_RESPONSE = "";
 
     private TreeNode root;
 
@@ -25,9 +26,36 @@ public class DictionaryTree {
         this(responseTemplates.toArray(new ResponseTemplate[0]));
     }
 
-    //TODO
+    /**
+     * Searches the DictionaryTree for the given prompt to find the leaf node associated with the prompt which contains a
+     * response.
+     * @param prompt the prompt that is used to search the DictionaryTree
+     * @return The response associated with a leaf node if found. Otherwise returns {@link DictionaryTree#NO_RESPONSE}
+     */
     public String search(String[] prompt) {
-        return NO_RESPONSE;
+        if(prompt == null) {
+            return NO_RESPONSE;
+        }
+        String response = NO_RESPONSE;
+        TreeNode node = root;
+
+        for (int i = 0; i < prompt.length; i++) {
+            String word = prompt[i];
+            node = node.getChild(word);
+
+            //Check if there is a new node, breaks out of the loop if there is no new node
+            if(node == null) {
+                break;
+            }
+
+            //Checks to see if the loop is at the end of the prompt and the last found node is a leaf node.
+            //Sets the response as the first response in the node
+            if(i == prompt.length - 1 && node.isLeaf()) {
+                response = node.getResponse();
+            }
+        }
+
+        return response;
     }
 
     //Creates the Dictionary Tree
